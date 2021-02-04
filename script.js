37c99b13664eb4460d742042747ce8fed799ebad
jeffry delapena <jeffrydelapena@jeffrys-air.lan>
2021-02-03 18:19:13 -0600

    redblack

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Your JS goes here
+// Your JS goes here\
+var x;
+let board = document.querySelector("body")
+for(var i = 0; i < 81; i++) {
+ var div = document.createElement("div")
+//board.appendChild(div) 
+var b = document.querySelector("div")
+div.style.width = "11.1%"
+div.style.cssFloat = "left"
+div.style.paddingBottom = "11.1%"
+if(x) {
+div.style.backgroundColor = "black"
+x = false
+} else {
+div.style.backgroundColor = "red" 
+x = true 
+}
+board.appendChild(div)
+}
